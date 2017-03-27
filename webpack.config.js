668310428e621314989959c25a32f7c54ebcc79c
Roman_Home <roman.marchenko@saltpepper.ru>
2017-03-27 19:04:20 +0300

    - init project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+///
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: './public/build.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+              fallback: "style-loader",
+              use: "css-loader?importLoaders=1!postcss-loader",
+            }),
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', "stage-0", 'react']
+        }
+      },
+    ]
+  },
+
+  plugins: [
+      new ExtractTextPlugin("./public/style.css"),
+    ]
+}
